4a37095672f1def9a63490332609715020683f9c
Alexander Kraev <lexkraev@gmail.com>
2023-02-28 12:26:49 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 
 import PackageDescription
 
-let frameworks = [  "CardKitCore-bitcode-no": "a7ccc99fe180c3282d2634b092b3bb0ad855fc1c48439a71ceaa37ae2ca59942",
-                    "ThreeDSSDK-bitcode-no": "6db4f731c01fae44b0f4528e6a39c3405b46634573cc6c7504c9842a535fd3d3",
-                    "CardKit-bitcode-no": "1884d624560bb51762bdfb5ffe0616e1ade14604082716b6bc3258c5e0cd570c"  ]
+let frameworks = [  "CardKitCore": "a7ccc99fe180c3282d2634b092b3bb0ad855fc1c48439a71ceaa37ae2ca59942",
+                    "ThreeDSSDK": "6db4f731c01fae44b0f4528e6a39c3405b46634573cc6c7504c9842a535fd3d3",
+                    "CardKit": "1884d624560bb51762bdfb5ffe0616e1ade14604082716b6bc3258c5e0cd570c"  ]
 
 let package = Package(
     name: "CardKitSDK",
