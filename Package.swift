8050fd510b55878820508d88140b0b072f5eb8c1
Alexander Kraev <lexkraev@gmail.com>
2023-03-02 12:47:02 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,13 +2,14 @@
 
 import PackageDescription
 
-let frameworks = ["ThreeDSSDK": "203f17f02c285b3d3e88278a55e4138c2410a126dc692fa910e870480afd9af4",
-                  "CardKit": "c8643189e76009bc13b94196fb02d70bffe77d557423bfa799ebb7fabf5dbada",
-                  "CardKitCore": "5c45aeab5d23b060b82ed87a13ccaec7c20145b61305a2676dd422e8d3bbb437"]
+let frameworks = ["CardKitPayment": "6be6cf2c85aa13cccd856804daedcc5e06b0cdc2006210808053ad4bf2d26bcb",
+                  "ThreeDSSDK": "a201512aa85650987543b075aebd77fe92f59cd37ca025f0ed7f3bec25d1f0e4",
+                  "CardKit": "364ee46e45334742dda292dbfb857b5318ced85bd06c096d4b82c5f2a5971012",
+                  "CardKitCore": "1b013f28ac10a91fe2eb308d6903ef3679e730c58adb9cd5eb5a9223550cc9c9" ]
 
 let package = Package(
     name: "CardKitSDK",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -22,7 +23,7 @@ let package = Package(
     ],
     targets: frameworks.map { name, checksum in
             .binaryTarget(name: name,
-                          url: "https://github.com/c-villain/CardKit/releases/download/0.0.20/\(name).xcframework.zip",
+                          url: "https://github.com/c-villain/CardKit/releases/download/2.0.4/\(name).xcframework.zip",
                           checksum: checksum)
     }
 )
